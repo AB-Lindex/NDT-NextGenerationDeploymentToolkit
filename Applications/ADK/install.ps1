00f4e5000c7229156f7f65e293ad50112ad61bcd
Leif <leif.almberg@lindex.com>
2026-03-05 15:24:56 +0100

    26-03-05 15:24:56

--- Applications/ADK/install.ps1
+++ Applications/ADK/install.ps1
@@ -1,4 +1,7 @@
 Set-Location $PSScriptRoot
 
+Unblock-File -Path ".\adksetup.exe"
+Unblock-File -Path ".\adkwinpesetup.exe"
+
 Start-Process -FilePath ".\adksetup.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process -FilePath ".\adkwinpesetup.exe" -ArgumentList "/quiet /norestart" -Wait
