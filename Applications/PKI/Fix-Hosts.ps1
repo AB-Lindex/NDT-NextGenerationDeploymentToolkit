9344477918299314f54d03ef3cbd1f23736b4ae4
Leif <leif.almberg@lindex.com>
2026-04-12 20:20:38 +0200

    26-04-12 20:20:38

--- Applications/PKI/Fix-Hosts.ps1
+++ Applications/PKI/Fix-Hosts.ps1
@@ -1,2 +1,1 @@
-Add-Content -Path "c:\Windows\system32\drivers\etc\hosts" -Value "10.0.3.11 dc01.corp.dev" -Force
-# cmd /c echo 10.0.3.11 dc01.corp.dev >> %OSDISK%\windows\system32\drivers\etc\hosts
+Add-Content -Path "c:\Windows\system32\drivers\etc\hosts" -Value "10.0.3.16 ndt01.corp.dev" -Force
