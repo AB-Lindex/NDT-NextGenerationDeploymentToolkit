5bf853b0af4dc6438879878ec352283a3a708143
Leif <leif.almberg@lindex.com>
2026-07-27 21:55:40 +0200

    26-07-27 21:55:39

--- Applications/NDT Setup/install.ps1
+++ Applications/NDT Setup/install.ps1
@@ -1,4 +1,7 @@
 install-module -Name ndt -Force -Scope AllUsers
-Install-NDT -DeployPassword (ConvertTo-SecureString -AsPlainText -Force -String 'P@ssw0rd2026')
+Install-NDT `
+ -DeployPassword (ConvertTo-SecureString -AsPlainText -Force -String 'P@ssw0rd2026') `
+ -DeployUser 'Corp\Deploy2026' `
+ -skipmonitor
 
 New-NDTPEImage
